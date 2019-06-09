5a82d94226ec7c716ec166d758865e18bbf4f422
GeonLee <leegun2003@gmail.com>
2019-06-10 00:22:44 +0900

    update: prototype

--- js/object_factory.js
+++ js/object_factory.js
@@ -3,11 +3,18 @@ function Person(name, first, second, third) {
   this.first = first;
   this.second = second;
   this.third = third;
-  this.sum = function() {
-    return this.first + this.second + this.third;
-  };
 }
+
+/* necessity of prototype */
+Person.prototype.sum = function() {
+  return this.first + this.second + this.third;
+};
 
 var kim = new Person("kim", 10, 20, 30);
 var lee = new Person("lee", 10, 10, 10);
+
+kim.sum = function() {
+  return this.first + this.second;
+};
+
 console.log(kim.sum(), lee.sum());
