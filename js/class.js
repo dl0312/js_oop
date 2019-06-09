ed29274f1ac1f47b78c5e5688e23f9473bce3ae6
GeonLee <leegun2003@gmail.com>
2019-06-10 00:45:00 +0900

    update: class inheritance

--- js/class.js
+++ js/class.js
@@ -3,16 +3,32 @@ class Person {
     this.name = name;
     this.first = first;
     this.second = second;
+  }
+  sum() {
+    return this.first + this.second;
+  }
+}
+
+class Student extends Person {
+  constructor(name, first, second, third) {
+    super(name, first, second);
     this.third = third;
   }
   sum() {
     return this.first + this.second + this.third;
   }
+  avg() {
+    return (this.first + this.second + this.third) / 3;
+  }
 }
 
-var kim = new Person("kim", 10, 20, 30);
+var kim = new Person("kim", 10, 20);
 console.log(kim);
 console.log(kim.sum());
-var lee = new Person("lee", 10, 10, 10);
+var lee = new Person("lee", 10, 10);
 console.log(lee);
 console.log(lee.sum());
+var park = new Student("park", 0, 50, 100);
+console.log(park);
+console.log(park.sum());
+console.log(park.avg());
